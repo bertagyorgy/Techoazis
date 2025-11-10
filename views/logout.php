e7f465a829fec402ef11e444b671a5eeaba0bdd5
Berta György <bertagyurci14@gmail.com>
2025-11-10 14:05:46 +0100

    finally working email

--- views/logout.php
+++ views/logout.php
@@ -12,5 +12,5 @@ if (isset($_SESSION['user_id'])) {
 }
 
 // Biztonságosabb header-es redirect
-header("Location: ../index.php");
+echo "<script>window.location.href='../index.php';</script>";
 exit();
