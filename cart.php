22183faaf766dbf12300acaa786aec92cd5cb905
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2025-12-06 18:13:45 +0100

    adding index.js to cart.php

--- cart.php
+++ cart.php
@@ -105,7 +105,8 @@ if (!empty($cart) && isset($conn)) {
 ?>
 
 <link rel="stylesheet" href="./static/index.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+<script src="./static/index.js" defer></script>
 
 <style>
 
