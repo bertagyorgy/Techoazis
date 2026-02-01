0e7194a354d47ec980f59e2e0bbdffa75541d202
papmatedaniel <papmd2014@gmail.com>
2026-02-01 14:34:55 +0100

    contact path correction

--- contact.php
+++ contact.php
@@ -9,38 +9,38 @@ require_once __DIR__ . '/config.php';
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Techoázis | Kapcsolat</title>
-    <link rel="icon" type="image/x-icon" href="./images/palmtree_favicon.svg">
+    <link rel="icon" type="image/x-icon" href="<?= BASE_URL ?>/images/palmtree_favicon.svg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
-    <link rel="stylesheet" href="./static/index.css">
-    <link rel="stylesheet" href="./static/animations_microinteractions.css">
-    <link rel="stylesheet" href="./static/button_system.css">
-    <link rel="stylesheet" href="./static/comments.css">
-    <link rel="stylesheet" href="./static/create_post.css">
-    <link rel="stylesheet" href="./static/forum.css">
-    <link rel="stylesheet" href="./static/group_view.css">
-    <link rel="stylesheet" href="./static/modern_navbar.css">
-    <link rel="stylesheet" href="./static/post_card.css">
-    <link rel="stylesheet" href="./static/responsive_adjustments.css">
-    <link rel="stylesheet" href="./static/reset&base_styles.css">
-    <link rel="stylesheet" href="./static/container&grid_system.css">
-    <link rel="stylesheet" href="./static/custom_card.css">
-    <link rel="stylesheet" href="./static/feature_cards.css">
-    <link rel="stylesheet" href="./static/hero_section.css">
-    <link rel="stylesheet" href="./static/loading_animation.css">
-    <link rel="stylesheet" href="./static/modern_footer.css">
-    <link rel="stylesheet" href="./static/utility_classes.css">
-    <link rel="stylesheet" href="./static/contact_style.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/index.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/animations_microinteractions.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/button_system.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/comments.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/create_post.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/forum.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/group_view.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/modern_navbar.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/post_card.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/responsive_adjustments.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/reset&base_styles.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/container&grid_system.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/custom_card.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/feature_cards.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/hero_section.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/loading_animation.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/modern_footer.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/utility_classes.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/static/contact_style.css">
 
     <!-- Inter font hozzáadása -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <script src="./static/index.js" defer></script>
-    <script src="./static/forum.js" defer></script>
+    <script src="<?= BASE_URL ?>/static/index.js" defer></script>
+    <script src="<?= BASE_URL ?>/static/forum.js" defer></script>
 
 </head>
 <body>
-<?php include './views/navbar.php'; ?>
+<?php include ROOT_PATH . '/views/navbar.php'; ?>
 
 <main class="contact-page">
   <div class="contact-container">
@@ -87,7 +87,7 @@ require_once __DIR__ . '/config.php';
         </div>
       <?php endif; ?>
 
-      <form action="./app/send_contact.php" method="POST">
+      <form action="<?= BASE_URL ?>/app/send_contact.php" method="POST">
         <div class="form-group">
           <label for="name">Név</label>
           <input id="name" name="name" type="text" class="form-control" required minlength="2" />
@@ -161,11 +161,11 @@ require_once __DIR__ . '/config.php';
             <div class="grid-col-4 footer-nav">
                 <h3 class="footer-title">Navigáció</h3>
                 <ul class="footer-links">
-                    <li><a href="./index.php" class="footer-link"><i class="fas fa-home"></i> Főoldal</a></li>
-                    <li><a href="./shop.php" class="footer-link"><i class="fas fa-shopping-cart"></i> Webshop</a></li>
-                    <li><a href="./forum.php" class="footer-link"><i class="fas fa-comments"></i> Csevegés</a></li>
-                    <li><a href="./articles.php" class="footer-link"><i class="fa-solid fa-pen"></i>Cikkek</a></li>
-                    <li><a href="./about_us.php" class="footer-link"><i class="fa-solid fa-address-card"></i>Rólunk</a></li>
+                    <li><a href="<?= BASE_URL ?>/index.php" class="footer-link"><i class="fas fa-home"></i> Főoldal</a></li>
+                    <li><a href="<?= BASE_URL ?>/shop.php" class="footer-link"><i class="fas fa-shopping-cart"></i> Webshop</a></li>
+                    <li><a href="<?= BASE_URL ?>/forum.php" class="footer-link"><i class="fas fa-comments"></i> Csevegés</a></li>
+                    <li><a href="<?= BASE_URL ?>/articles.php" class="footer-link"><i class="fa-solid fa-pen"></i>Cikkek</a></li>
+                    <li><a href="<?= BASE_URL ?>/about_us.php" class="footer-link"><i class="fa-solid fa-address-card"></i>Rólunk</a></li>
                     <?php
                     if (isset($_SESSION['user_id']) && isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'A'): ?>
                         <li><a href="admin/admin.php" class="footer-link"><i class="fas fa-cog"></i> Admin</a></li>
