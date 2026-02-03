2b5f527efd9acd360c5b2078bda003f41e293bec
Berta György <bertagyurci14@gmail.com>
2026-02-03 11:41:00 +0100

    icons link readded

--- index.php
+++ index.php
@@ -76,6 +76,7 @@ if ($page !== '' && $page !== 'index') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <script src="<?= BASE_URL ?>/static/index.js" defer></script>
 
 </head>
